433b40e0d3b587734097bdc6c580313ce4f6afd5
kita456 <g140702@gmail.com>
2025-04-14 23:30:16 +0900

    add

--- web/src/components/Welcome/Welcome.tsx
+++ web/src/components/Welcome/Welcome.tsx
@@ -3,17 +3,15 @@ import classes from './Welcome.module.css';
 
 export function Welcome() {
   return (
-    <>
-      <Title className={classes.title} ta="center" mt={20}>
-        <Text
-          inherit
-          variant="gradient"
-          component="span"
-          gradient={{ from: '#326ce5', to: '#94c6ff' }}
-        >
-          Kubernetes Localization Status
-        </Text>
-      </Title>
-    </>
+    <Title className={classes.title} ta="center" mt={20}>
+      <Text
+        inherit
+        variant="gradient"
+        component="span"
+        gradient={{ from: '#326ce5', to: '#94c6ff' }}
+      >
+        Kubernetes Localization Status
+      </Text>
+    </Title>
   );
 }
