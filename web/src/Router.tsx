8aa844de25f0b0db0b05bb7b764c45b2012452ab
kita456 <g140702@gmail.com>
2025-04-20 15:37:01 +0000

    rename repo name

--- web/src/Router.tsx
+++ web/src/Router.tsx
@@ -2,14 +2,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 import { TermsOfServicePage } from './pages/TermsOfService.page';
 
+const basename = import.meta.env.MODE === 'production' ? '/kube-i18n-status' : '/';
+
 const router = createBrowserRouter(
   [
     { path: '/', element: <HomePage /> },
     { path: '/terms', element: <TermsOfServicePage /> },
   ],
-  {
-    basename: '/kube-i18n-status',
-  }
+  { basename }
 );
 
 export function Router() {
